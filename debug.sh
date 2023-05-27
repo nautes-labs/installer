eebf3a6e86fe18e50e16051a752210add9133360
cty <xenoRegex@outlook.com>
2023-05-27 18:16:33 +0800

    change default image version to latest

--- debug.sh
+++ debug.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 NAUTES_PATH="/opt/nautes"
+
 CONTAINER_NAME=nautes-installer
+if [ -z "$INSTALLER_VERSION" ]
+then
+    INSTALLER_VERSION="latest"
+fi
+
 
 if ! [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
   docker run -d --name $CONTAINER_NAME \
@@ -12,7 +18,7 @@ if ! [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
     -v `pwd`/nautes:/opt/nautes \
     -v `pwd`/ansible-vault:/opt/nautes/roles/ansible-vault \
     -v `pwd`/vars.yaml:/opt/vars.yaml \
-    ghcr.io/nautes-labs/installer:v0.2.0 \
+    ghcr.io/nautes-labs/installer:${INSTALLER_VERSION} \
       tail -f /dev/null
 fi
 
