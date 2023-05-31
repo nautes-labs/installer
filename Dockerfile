42801cc389a74debdc699f36bd691a9d59cb49d0
cty <xenoRegex@outlook.com>
2023-05-31 13:49:48 +0800

    [refactor] Integrate all scripts into installer.sh

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,7 @@ ENV PATH=$PATH:/opt/bin
 COPY bin /opt/bin
 
 RUN set -x \
- && cd /tmp \
- && apk add --no-cache git zip terraform vim openssh py3-pip gcc libc-dev python3-dev openssl-dev helm tree curl tar bash \
+ && apk add --no-cache git zip terraform vim openssh py3-pip gcc libc-dev python3-dev openssl-dev helm tree curl tar bash jq yq \
  && echo "set mouse=" > /root/.vimrc \
  && echo -e '\
 Host *\n\
@@ -14,21 +13,18 @@ Host *\n\
     UserKnownHostsFile /dev/null\n\
     IdentityFile /opt/out/hosts/id_rsa'\
 >> /etc/ssh/ssh_config \
- && curl -OL https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl \
- && chmod +x ./kubectl \
- && mv ./kubectl /usr/local/bin/kubectl \
  && git clone --depth=1 https://github.com/kubernetes-sigs/kubespray.git --branch v2.19.1 /opt/kubespray \
- && pip install -r /opt/kubespray/requirements.txt \
- && ansible-galaxy install geerlingguy.gitlab \
- && curl -OL https://github.com/nautes-labs/init-vault/releases/download/v0.2.0/init-vault_linux_amd64.tar.gz \
- && tar zxf init-vault_linux_amd64.tar.gz \
- && mv init-vault /opt/bin \
- && rm /tmp/*
+ && pip install -r /opt/kubespray/requirements.txt 
+
+RUN set -x \
+ && curl -o /usr/local/bin/kubectl -L https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl \
+ && chmod +x /usr/local/bin/kubectl \
+ && curl -L https://github.com/nautes-labs/init-vault/releases/download/v0.2.0/init-vault_linux_amd64.tar.gz | tar zx -C /opt/bin 
 
 COPY nautes /opt/nautes
 RUN set -x \
- && pip install -r /opt/nautes/requirements.txt \
- && git clone --depth=1 https://github.com/ansible-community/ansible-vault.git /opt/nautes/roles/ansible-vault
+ && pip install -r /opt/nautes/requirements.txt
 
 WORKDIR /opt
 
+ 
---
+++ installer.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+set -x
+
+CONTAINER_NAME=nautes-installer
+if [ -z "$INSTALLER_VERSION" ]
+then
+    INSTALLER_VERSION="latest"
+fi
+
+NAUTES_PATH="/opt/nautes"
+NAUTES_LOG_PATH="${NAUTES_PATH}/out/logs"
+NAUTES_REPO_PATH="${NAUTES_PATH}/repos"
+
+function git_clone() {
+    if ! [ $# -eq 3 ]
+    then
+        echo "Insufficient parameters, git clone failed."
+        exit 1
+    fi
+
+    local TARGET_DIR="${NAUTES_REPO_PATH}/$1"
+    local REVISION=$2
+    local URL=$3
+    
+    if [ -d "$TARGET_DIR/.git" ]; then
+        echo "Repository $1 already exists, updating to the latest code..."
+        git --git-dir="$TARGET_DIR/.git" --work-tree="$TARGET_DIR" pull
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to fetch updates from the remote repository."
+            exit 1
+        fi
+    else
+        echo "Repository $1 not found, cloning to the $TARGET_DIR directory..."
+        echo "========================="
+        echo "Using revision $REVISION "
+        echo "========================="
+        git clone --depth 1 -b $REVISION "$URL" "$TARGET_DIR"
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to clone the repository."
+            exit 1
+        fi
+    fi
+}
+
+function init() {
+    mkdir -p ${NAUTES_LOG_PATH}
+    mkdir -p ${NAUTES_REPO_PATH}
+
+    if [ -z "$TENANT_REPO_TEMPLATE_REVISION" ]
+    then
+        TENANT_REPO_TEMPLATE_REVISION="main"
+    fi
+    local TENANT_REPO_URL="https://github.com/nautes-labs/tenant-repo-template.git"
+
+    git_clone "management" ${TENANT_REPO_TEMPLATE_REVISION} ${TENANT_REPO_URL}
+
+    if [ -z "${ANSIBLE_ROLE_VAULT_REVISION}" ]
+    then
+        ANSIBLE_ROLE_VAULT_REVISION="master"
+    fi
+    local ANSIBLE_ROLE_VAULT_URL="https://github.com/ansible-community/ansible-vault.git"
+
+    git_clone "ansible-vault" ${ANSIBLE_ROLE_VAULT_REVISION} ${ANSIBLE_ROLE_VAULT_URL}
+
+    if [ -z "${ANSIBLE_ROLE_GITLAB_REVISION}" ]
+    then
+        ANSIBLE_ROLE_GITLAB_REVISION="master"
+    fi
+    local ANSIBLE_ROLE_GITLAB_URL="https://github.com/geerlingguy/ansible-role-gitlab.git"
+
+    git_clone "gitlab" ${ANSIBLE_ROLE_GITLAB_REVISION} ${ANSIBLE_ROLE_GITLAB_URL}
+}
+
+function run_container() {
+    local EXTRA_MOUNT="-v ${NAUTES_REPO_PATH}/management:/opt/management \
+-v ${NAUTES_REPO_PATH}/ansible-vault:/opt/nautes/roles/ansible-vault \
+-v ${NAUTES_REPO_PATH}/gitlab:/opt/nautes/roles/gitlab"
+
+    if [ "$1" = "debug" ]; then
+        EXTRA_MOUNT=" -v `pwd`/nautes:/opt/nautes \
+-v `pwd`/bin:/opt/bin \
+${EXTRA_MOUNT}"
+    elif [ "$1" = "destory" ]; then
+        EXTRA_MOUNT=""
+    fi
+
+    if ! [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+        docker run -d --name ${CONTAINER_NAME} ${EXTRA_MOUNT}\
+         -v ${NAUTES_PATH}/out:/opt/out \
+         -v ${NAUTES_PATH}/terraform:/tmp/terraform \
+         -v `pwd`/vars.yaml:/opt/vars.yaml \
+         ghcr.io/nautes-labs/installer:${INSTALLER_VERSION} \
+           tail -f /dev/null
+    fi
+}
+
+function destory() {
+    set -e
+    set -o pipefail
+    docker exec $CONTAINER_NAME destroy-hosts   | tee /tmp/destroy.log 
+    docker rm -f $CONTAINER_NAME                | tee -a /tmp/destroy.log
+    rm -rvf $NAUTES_PATH                        | tee -a /tmp/destroy.log
+}
+
+function install() {
+    local LOG_FILE="${NAUTES_LOG_PATH}/install.log"
+    rm $LOG_FILE && touch $LOG_FILE
+    
+    local INSTALLATION_PROGRESS_PATH="${NAUTES_PATH}/flags"
+    mkdir -p ${INSTALLATION_PROGRESS_PATH}
+
+    local FLAG_CREATION_COMPLETED_HOST="${INSTALLATION_PROGRESS_PATH}/create_host"
+    if ! [ -e $FLAG_CREATION_COMPLETED_HOST ]; then
+        docker exec -i $CONTAINER_NAME create-hosts | tee -a $LOG_FILE 
+        touch $FLAG_CREATION_COMPLETED_HOST
+    fi
+
+    local FLAG_INSTALLATION_COMPLETED_KUBERNETES="${INSTALLATION_PROGRESS_PATH}/kubernetes"
+    if ! [ -e $FLAG_INSTALLATION_COMPLETED_KUBERNETES ]; then
+        docker exec -i $CONTAINER_NAME install-k8s | tee -a $LOG_FILE 
+        touch $FLAG_INSTALLATION_COMPLETED_KUBERNETES
+    fi
+
+    docker exec -i $CONTAINER_NAME install-nautes | tee -a $LOG_FILE 
+}
+
+
+if [ "$1" == "debug" ]; then
+    set -e
+    init
+    curl -L https://github.com/nautes-labs/init-vault/releases/download/v0.2.0/init-vault_linux_amd64.tar.gz | tar zx -C ./bin
+    run_container "debug"
+    docker exec -it $CONTAINER_NAME sh
+elif [ "$1" == "destory" ]; then
+    run_container "destory"
+    destory 
+else
+    init
+    run_container 
+    install
+fi
+
