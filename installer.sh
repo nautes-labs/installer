60665a8d89f8aa6ab6ca16de44d60d38fbcb0f3d
cty <xenoRegex@outlook.com>
2023-05-31 20:01:59 +0800

    [feat] support k3s install

--- installer.sh
+++ installer.sh
@@ -80,7 +80,7 @@ function run_container() {
         EXTRA_MOUNT=" -v `pwd`/nautes:/opt/nautes \
 -v `pwd`/bin:/opt/bin \
 ${EXTRA_MOUNT}"
-    elif [ "$1" = "destory" ]; then
+    elif [ "$1" = "destroy" ]; then
         EXTRA_MOUNT=""
     fi
 
@@ -94,7 +94,7 @@ ${EXTRA_MOUNT}"
     fi
 }
 
-function destory() {
+function destroy() {
     set -e
     set -o pipefail
     docker exec $CONTAINER_NAME destroy-hosts   | tee /tmp/destroy.log 
@@ -131,9 +131,9 @@ if [ "$1" == "debug" ]; then
     curl -L https://github.com/nautes-labs/init-vault/releases/download/v0.2.0/init-vault_linux_amd64.tar.gz | tar zx -C ./bin
     run_container "debug"
     docker exec -it $CONTAINER_NAME sh
-elif [ "$1" == "destory" ]; then
-    run_container "destory"
-    destory 
+elif [ "$1" == "destroy" ]; then
+    run_container "destroy"
+    destroy 
 else
     init
     run_container 
